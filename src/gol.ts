437b2b21b0edc44e8209f70f52e64913943d589c
Nicole LeGare <dlegare.1001@gmail.com>
2025-04-25 19:01:32 -0700

    Remove some indentation

--- src/gol.ts
+++ src/gol.ts
@@ -3,56 +3,56 @@ export const GRID_SIZE = 1024;
 const WORKGROUP_SIZE = 8;
 
 const GOL_SHADER = `
-    @group(0) @binding(0) var<uniform> grid: vec2f;
+@group(0) @binding(0) var<uniform> grid: vec2f;
 
-    @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
-    @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
+@group(0) @binding(1) var<storage> cellStateIn: array<u32>;
+@group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
 
-    // Maps the cell coordinates to the index in the storage buffer,
-    // wrapping within the grid (we living on a taurus baybeeeeeeeee).
-    fn cellIndex(cell: vec2u) -> u32 {
-        return (cell.y % u32(grid.y)) * u32(grid.x) +
-        (cell.x % u32(grid.x));
-    }
+// Maps the cell coordinates to the index in the storage buffer,
+// wrapping within the grid (we living on a taurus baybeeeeeeeee).
+fn cellIndex(cell: vec2u) -> u32 {
+    return (cell.y % u32(grid.y)) * u32(grid.x) +
+    (cell.x % u32(grid.x));
+}
 
-    fn cellActive(x: u32, y: u32) -> u32 {
-        return cellStateIn[cellIndex(vec2(x, y))];
-    }
+fn cellActive(x: u32, y: u32) -> u32 {
+    return cellStateIn[cellIndex(vec2(x, y))];
+}
 
-    @compute
-    @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
-    fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
-        // Determine how many active neighbors this cell has.
-        let activeNeighbors =
-        cellActive(cell.x + 1, cell.y + 1) +
-        cellActive(cell.x + 1, cell.y    ) +
-        cellActive(cell.x + 1, cell.y - 1) +
-        cellActive(cell.x,     cell.y - 1) +
-        cellActive(cell.x - 1, cell.y - 1) +
-        cellActive(cell.x - 1, cell.y    ) +
-        cellActive(cell.x - 1, cell.y + 1) +
-        cellActive(cell.x,     cell.y + 1);
+@compute
+@workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
+fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
+    // Determine how many active neighbors this cell has.
+    let activeNeighbors =
+    cellActive(cell.x + 1, cell.y + 1) +
+    cellActive(cell.x + 1, cell.y    ) +
+    cellActive(cell.x + 1, cell.y - 1) +
+    cellActive(cell.x,     cell.y - 1) +
+    cellActive(cell.x - 1, cell.y - 1) +
+    cellActive(cell.x - 1, cell.y    ) +
+    cellActive(cell.x - 1, cell.y + 1) +
+    cellActive(cell.x,     cell.y + 1);
 
-        let i = cellIndex(cell.xy);
+    let i = cellIndex(cell.xy);
 
-        // Conway's game of life rules:
-        switch activeNeighbors {
-            // Active cells with 2 neighbors stay active.
-            case 2: {
-                cellStateOut[i] = cellStateIn[i];
-            }
+    // Conway's game of life rules:
+    switch activeNeighbors {
+        // Active cells with 2 neighbors stay active.
+        case 2: {
+            cellStateOut[i] = cellStateIn[i];
+        }
 
-            // Cells with 3 neighbors become or stay active.
-            case 3: {
-                cellStateOut[i] = 1;
-            }
+        // Cells with 3 neighbors become or stay active.
+        case 3: {
+            cellStateOut[i] = 1;
+        }
 
-            // Cells with < 2 or > 3 neighbors become inactive.
-            default: {
-                cellStateOut[i] = 0;
-            }
+        // Cells with < 2 or > 3 neighbors become inactive.
+        default: {
+            cellStateOut[i] = 0;
         }
     }
+}
 `;
 
 export class LifeSimulation {
